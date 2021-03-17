01188972b3e579e65b98f9eded80508a9638aa33
imramzan <77074717+imramzan@users.noreply.github.com>
2021-03-17 01:16:48 -0400

    uploading final version of Pypoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,17 +4,17 @@ Created on Thu Mar  4 12:11:39 2021
 
 @author: ramza
 """
+#import necessary Python modules
 import os
 import csv
-csvpath = os.path.join('PyBank/Raw_Data/PyBank_data.csv')
 
-#with open (csvpath, 'r') as csv_reader:
+csvpath = os.path.join("PyBank/Raw_Data/PyBank_data.csv")
+
 with open (csvpath, newline="") as csvfile:
     csv_reader =csv.reader(csvfile, delimiter=",")
     csv_headers = next(csv_reader, None)
 
-# count the months
-# 1. create lists to store months and profitLoss
+# create lists to store months and profitLoss
     months = []
     profit = []
 
@@ -24,15 +24,15 @@ with open (csvpath, newline="") as csvfile:
 
     total_months = len(months)
     print (f"Total Months: {total_months}")
-# add the profit/Loss to get total revenue
+
+# add the profit/Loss to get total revenues
     revenues = 0
     i = 1
     for i in range(total_months):
         revenues = revenues + int(profit[i])
     print (f"Total Revenue: ${revenues}")
 
-# find the average change per month
-# 1. find change in profit/loss per month
+# find change in profit/loss per month
     change = []
     j = 0
     k = 0
@@ -43,28 +43,23 @@ with open (csvpath, newline="") as csvfile:
         else:
             change.append(int(profit[j])-int(profit[k]))
             k += 1
-    #print (change)
-# 2. sum the monthly changes and divide by total_months
+
+# 2. find the average change per month: sum the monthly changes and divide by total number of months
     average_month = ((sum(change))/(len(change)))
     print (f"Average Change: ${round((average_month),2)}")
-       
-# greatest increase in profits
+
+# find greatest increase in profits
     max_change = max(change)
     print (f"Greatest Increase in Revenues: ${max_change}")
-# greatest decrease in profits
+# find greatest decrease in profits
     min_change = min(change)
     print (f"Greasest Decrease in Revenues:  ${min_change}")
 
-# export results
-exportpath = ("Results.txt")
+# export results to text file
+exportpath = ("PyBank/PyBank_Results.txt")
 with open(exportpath, "w") as textfile:
-        textfile.write(f"Total Months: {total_months}")
-        textfile.write("-----------------------------")
-        textfile.write(f"Total Revenue: ${revenues}")
-        textfile.write("-----------------------------")
-        textfile.write(f"Average Change: ${round((average_month),2)}")
-        textfile.write("-----------------------------")
-        textfile.write(f"Greatest Increase in Revenues: ${max_change}")
-        textfile.write("-----------------------------")
-        textfile.write(f"Greasest Decrease in Revenues:  ${min_change}")    
-
+        textfile.write(f"Total Months: {total_months}" + "\n")
+        textfile.write(f"Total Revenue:  ${revenues}" + "\n")
+        textfile.write(f"Average Change: ${round((average_month),2)}" + "\n")
+        textfile.write(f"Greatest Increase in Revenues: ${max_change}" + "\n")
+        textfile.write(f"Greasest Decrease in Revenues:  ${min_change}" + "\n")
