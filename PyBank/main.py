41657422bfaa9b792d18e35a25c9aae6a09a6f3d
imramzan <77074717+imramzan@users.noreply.github.com>
2021-03-16 23:30:59 -0400

    Code retest

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,14 +6,12 @@ Created on Thu Mar  4 12:11:39 2021
 """
 import os
 import csv
-csvpath = os.path.join('Raw_Data/PyBank_data.csv')
+csvpath = os.path.join('PyBank/Raw_Data/PyBank_data.csv')
 
+#with open (csvpath, 'r') as csv_reader:
 with open (csvpath, newline="") as csvfile:
     csv_reader =csv.reader(csvfile, delimiter=",")
     csv_headers = next(csv_reader, None)
-#with open (csvpath, 'r') as csv_reader:
-#    lines = csv_reader.read()
-
 
 # count the months
 # 1. create lists to store months and profitLoss
@@ -26,12 +24,12 @@ with open (csvpath, newline="") as csvfile:
 
     total_months = len(months)
     print (f"Total Months: {total_months}")
-# add the profit/Loss to get revenues
+# add the profit/Loss to get total revenue
     revenues = 0
     i = 1
     for i in range(total_months):
         revenues = revenues + int(profit[i])
-    print (f"Total: ${revenues}")
+    print (f"Total Revenue: ${revenues}")
 
 # find the average change per month
 # 1. find change in profit/loss per month
@@ -49,14 +47,7 @@ with open (csvpath, newline="") as csvfile:
 # 2. sum the monthly changes and divide by total_months
     average_month = ((sum(change))/(len(change)))
     print (f"Average Change: ${round((average_month),2)}")
-
-# greatest increase in profits
-    max_change = max(change)
-    print (f"${max_change}")
-# greatest decrease in profits
-    min_change = min(change)
-    print (f"${min_change}")
-    
+       
 # greatest increase in profits
     max_change = max(change)
     print (f"Greatest Increase in Revenues: ${max_change}")
@@ -68,8 +59,12 @@ with open (csvpath, newline="") as csvfile:
 exportpath = ("Results.txt")
 with open(exportpath, "w") as textfile:
         textfile.write(f"Total Months: {total_months}")
-        textfile.write(f"Total: ${revenues}")
+        textfile.write("-----------------------------")
+        textfile.write(f"Total Revenue: ${revenues}")
+        textfile.write("-----------------------------")
         textfile.write(f"Average Change: ${round((average_month),2)}")
+        textfile.write("-----------------------------")
         textfile.write(f"Greatest Increase in Revenues: ${max_change}")
+        textfile.write("-----------------------------")
         textfile.write(f"Greasest Decrease in Revenues:  ${min_change}")    
 
